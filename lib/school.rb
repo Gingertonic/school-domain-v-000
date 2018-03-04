76047692f982bd8b2eff0a6124bd707c53571d55
Beth Schofield <bethmschofield@gmail.com>
2018-03-04 01:16:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster, :name
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+#NOT WORKING
+  def sort
+    binding.pry
+    @roster.each do |grade, students|
+      puts @roster[students].sort
+    end
+  end
+end
